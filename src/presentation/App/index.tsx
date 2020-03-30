a880e55eebd041b985ef8a39c6ae87e84965375a
gtobio11 <gtobio11@gmail.com>
2020-03-30 11:41:09 +0900

    [ADD] <polyfill, global style> good

--- src/presentation/App/index.tsx
+++ src/presentation/App/index.tsx
@@ -3,14 +3,23 @@ import PageData from '../../entities/PageData'
 import Footer from '../Footer';
 import RenderByPageData from '../../controller/RenderByPageData';
 import { Header } from '../';
+import { createGlobalStyle } from 'styled-components';
 
 import * as S from './Styles';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`
+
 
 const App = () => {
   const [ pageData, setPageData ] = useState(PageData.LANDING);
 
   return (
     <S.AppWrapper>
+      <GlobalStyle />
       <S.ContentWrapper>
         <S.ContentInnerWrapper>
           { pageData !== PageData.LANDING && <Header pageData={pageData}/>}
