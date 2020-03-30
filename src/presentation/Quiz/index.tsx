1f81b240347f73233f0ebfc496c89de17cb0e627
gtobio11 <gtobio11@gmail.com>
2020-03-30 18:05:20 +0900

    [FIX] quiz lint formatting

--- src/presentation/Quiz/index.tsx
+++ src/presentation/Quiz/index.tsx
@@ -13,80 +13,83 @@ interface QuizProps {
 }
 
 const Quiz = ({ setPageData }: QuizProps) => {
-  const [ selectedQuizValues, setSelectedQuizValues ] = useState<number[]>([]);
-  const [ quizzes, setQuizzes ] = useState<QuizData[]>([])
-  const [ currentPageIndex, setCurrentPageIndex ] = useState<number>(0);
+  const [selectedQuizValues, setSelectedQuizValues] = useState<number[]>([]);
+  const [quizzes, setQuizzes] = useState<QuizData[]>([])
+  const [currentPageIndex, setCurrentPageIndex] = useState<number>(0);
   const dispatch = useDispatch();
-  const { quizDatas, reduxSelectedQuizValues } = useSelector((state:StoreState) => ({ quizDatas: state.quiz.quizDatas, reduxSelectedQuizValues: state.quiz.selectedValues }))
+  const { quizDatas, reduxSelectedQuizValues } = useSelector((state: StoreState) => ({
+    quizDatas: state.quiz.quizDatas,
+    reduxSelectedQuizValues: state.quiz.selectedValues,
+  }));
 
   useEffect(() => {
-    if(!!quizDatas && quizDatas.length !== 0 && !!reduxSelectedQuizValues && reduxSelectedQuizValues.length !== 0) {
+    if ((quizDatas?.length ?? 0) !== 0 && (reduxSelectedQuizValues?.length ?? 0) !== 0) {
       setQuizzes(quizDatas);
       setSelectedQuizValues(reduxSelectedQuizValues);
       return;
     }
     const newQuizzes = [
       {
-        title: '퀴즈 아이템 테스트', 
-        choices: ['구구구', '구구구구', '구구구구구']
-      }, 
+        title: '퀴즈 아이템 테스트',
+        choices: ['구구구', '구구구구', '구구구구구'],
+      },
       {
         title: '두번째 퀴즈 아이템 테스트',
-        choices: ['멍멍멍', '멍멍멍멍', '멍멍멍멍멍', '멍멍멍멍멍멍']
+        choices: ['멍멍멍', '멍멍멍멍', '멍멍멍멍멍', '멍멍멍멍멍멍'],
       },
       {
         title: '세번째 퀴즈 아이템 테스트',
-        choices: ['야옹야옹', '야옹야옹야옹', '야옹야옹야옹야옹', '야옹야옹야옹야옹야옹', '야옹야옹야옹야옹야옹야옹']
-      }
+        choices: ['야옹야옹', '야옹야옹야옹', '야옹야옹야옹야옹', '야옹야옹야옹야옹야옹', '야옹야옹야옹야옹야옹야옹'],
+      },
     ];
     setQuizzes(newQuizzes);
     setSelectedQuizValues(new Array(newQuizzes.length).fill(-1));
-  }, [quizDatas, reduxSelectedQuizValues])
+  }, [quizDatas, reduxSelectedQuizValues]);
 
-  const handleQuizValues = useCallback((event:React.ChangeEvent<HTMLInputElement>, index:number) => {
+  const handleQuizValues = useCallback((event: React.ChangeEvent<HTMLInputElement>, index: number) => {
     const newSelected = [...selectedQuizValues];
     newSelected[index] = parseInt(event.target.value);
     setSelectedQuizValues(newSelected);
-  }, [selectedQuizValues])
+  }, [selectedQuizValues]);
 
   const handleClickController = useCallback((increase: number) => {
-    if((currentPageIndex + increase) < 0 || (currentPageIndex + increase) >= quizzes.length) {
+    if ((currentPageIndex + increase) < 0 || (currentPageIndex + increase) >= quizzes.length) {
       dispatch(setQuizData(quizzes, selectedQuizValues));
-      if((currentPageIndex + increase) < 0) {
+      if ((currentPageIndex + increase) < 0) {
         setPageData(PageData.EMAIL_INPUT);
-      } else if((currentPageIndex + increase) >= quizzes.length) {
+      } else if ((currentPageIndex + increase) >= quizzes.length) {
         setPageData(PageData.VOTE);
       }
       return false;
     }
     setCurrentPageIndex(currentPageIndex + increase)
-  }, [currentPageIndex, quizzes, setCurrentPageIndex, dispatch, selectedQuizValues, setPageData])
+  }, [currentPageIndex, quizzes, setCurrentPageIndex, dispatch, selectedQuizValues, setPageData]);
 
   return (
     <S.QuizWrapper>
       <S.QuizContentWrapper>
         {
-          !!quizzes && quizzes.length !== 0 && !!quizzes[currentPageIndex] && (
+          (quizzes?.length ?? 0) !== 0 && !!quizzes[currentPageIndex] && (
             <QuizItem
-            title={quizzes[currentPageIndex].title} 
-            choices={quizzes[currentPageIndex].choices}
-            selectedIndex={selectedQuizValues[currentPageIndex]}
-            handleQuizSelect={
-              (event: React.ChangeEvent<HTMLInputElement>) => handleQuizValues(event, currentPageIndex)
-            } />
+              title={quizzes[currentPageIndex].title}
+              choices={quizzes[currentPageIndex].choices}
+              selectedIndex={selectedQuizValues[currentPageIndex]}
+              handleQuizSelect={
+                (event: React.ChangeEvent<HTMLInputElement>) => handleQuizValues(event, currentPageIndex)
+              } />
           )
         }
       </S.QuizContentWrapper>
       <S.QuizButtonWrapper>
-        <S.QuizButton 
+        <S.QuizButton
           isNext={false}
           onClick={() => handleClickController(-1)}>
-            이전
+          이전
         </S.QuizButton>
-        <S.QuizButton 
+        <S.QuizButton
           isNext={true}
           onClick={() => handleClickController(1)}>
-            다음
+          다음
         </S.QuizButton>
       </S.QuizButtonWrapper>
     </S.QuizWrapper>
