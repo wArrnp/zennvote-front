0cd75a4dc2726223771cf09d1e58a74b462e49b6
gtobio11 <gtobio11@gmail.com>
2020-04-27 15:19:18 +0900

    [MODIFY] <quiz> input style

--- src/presentation/QuizItem/Styles.ts
+++ src/presentation/QuizItem/Styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+const checkedColor = "#3C9870"
+
 export const QuizItemWrapper = styled.div`
   display: inline-block;
   width: 100%;
@@ -10,14 +12,29 @@ export const QuizItemWrapper = styled.div`
 export const QuizItemTitle = styled.p`
   font-size: 20px;
   margin: 0;
-  margin-bottom: 10px;
+  margin-bottom: 25px;
 `;
 
 export const QuizItemLabel = styled.label`
-  display: block;
+  display: flex;
   cursor: pointer;
+  align-items: center;
+  margin-bottom: 10px;
 `;
 
 export const QuizItemRadioButton = styled.input`
-  margin-right: 10px;
+  margin: 0 10px 0 0;
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  border-radius: 100%;
+  background-color: white;
+  outline: none;
+  border: 1px solid grey;
+
+  &:checked {
+    appearance: none;
+    background-color: ${checkedColor};
+    border: none;
+  }
 `;
